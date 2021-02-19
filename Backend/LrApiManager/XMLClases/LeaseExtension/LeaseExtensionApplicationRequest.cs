650b15a5ce19ea496c9b5450cb69a0e5530233b9
Sachith <sachith.walpita90@gmail.com>
2021-02-19 14:28:07 +0530

    Lease extension

---
+++ Backend/LrApiManager/XMLClases/LeaseExtension/LeaseExtensionApplicationRequest.cs
@@ -0,0 +1,207 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LrApiManager.XMLClases.LeaseExtension
+{
+
+    public class LeaseExtensionApplicationRequest
+    {
+        public string AdditionalProviderFilter { get; set; }
+        public string MessageId { get; set; }
+        public string ExternalReference { get; set; }
+        public Product Product { get; set; }
+    }
+
+    public class Product
+    {
+        public string Reference { get; set; }
+        public int TotalFeeInPence { get; set; }
+        public string Email { get; set; }
+        public int TelephoneNumber { get; set; }
+        public bool AP1WarningUnderstood { get; set; }
+        public string ApplicationDate { get; set; }
+        public bool DisclosableOveridingInterests { get; set; }
+        public string PostcodeOfProperty { get; set; }
+        public Titles Titles { get; set; }
+        public List<Otherapplication> Applications { get; set; }
+        public List<Supportingdocument> SupportingDocuments { get; set; }
+
+        public Representations Representations { get; set; }
+        public List<Party> Parties { get; set; }
+        public string Notes { get; set; }
+        public string ApplicationAffects { get; set; }
+    }
+
+    public class Titles
+    {
+        public Leaseextension LeaseExtension { get; set; }
+    }
+
+    public class Leaseextension
+    {
+        public Lessortitles LessorTitles { get; set; }
+        public string LesseeTitle { get; set; }
+    }
+
+    public class Lessortitles
+    {
+        public string TitleNumber { get; set; }
+    }
+
+    public class Applications
+    {
+        public List<Otherapplication> OtherApplication { get; set; }
+    }
+
+    public class Otherapplication
+    {
+        public int Priority { get; set; }
+        public int Value { get; set; }
+        public int FeeInPence { get; set; }
+        public Document Document { get; set; }
+        public string Type { get; set; }
+    }
+
+    public class Document
+    {
+        public string CertifiedCopy { get; set; }
+    }
+
+    public class Supportingdocuments
+    {
+        public List<Supportingdocument> SupportingDocument { get; set; }
+        public List<Chargeapplication> ChargeApplication { get; set; }
+
+    }
+
+    public class Chargeapplication
+    {
+        public int Priority { get; set; }
+        public string Value { get; set; }
+        public int FeeInPence { get; set; }
+        public Document Document { get; set; }
+        public string ChargeDate { get; set; }
+        public string MDRef { get; set; }
+        public string SortCode { get; set; }
+    }
+
+    public class Supportingdocument
+    {
+        public string CertifiedCopy { get; set; }
+        public int DocumentId { get; set; }
+        public string DocumentName { get; set; }
+    }
+
+    public class Representations
+    {
+        public Lodgingconveyancer LodgingConveyancer { get; set; }
+        public Certified Certified { get; set; }
+    }
+
+    public class Lodgingconveyancer
+    {
+        public int RepresentativeId { get; set; }
+    }
+
+    public class Certified
+    {
+        public int RepresentativeId { get; set; }
+    }
+
+    public class Parties
+    {
+        public List<Party> Party { get; set; }
+    }
+
+    public class Party
+    {
+        public bool IsApplicant { get; set; }
+        public Person Person { get; set; }
+        public Company Company { get; set; }
+        public List<Role> Roles { get; set; }
+        public Addressforservice AddressForService { get; set; }
+
+    }
+
+    public class Person
+    {
+        public string Forenames { get; set; }
+        public string Surname { get; set; }
+    }
+
+    public class Roles
+    {
+        public List<Role> Role { get; set; }
+    }
+
+    public class Role
+    {
+        public string RoleType { get; set; }
+        public int Priority { get; set; }
+    }
+
+    public class Addressforservice
+    {
+        /* String values are A1, B1 or TA, where A1 is the
+           address of the property(A1 register), B1 is current
+           proprietor address(B1 register) and TA is address from
+           Transfer/Assent. */
+        public string AddressForServiceOption { get; set; }
+
+        public PostalAddress PostalAddress { get; set; }
+
+        public List<AdditionalAddresses> AdditionalAddresses { get; set; }
+    }
+
+    public class Company
+    {
+        public string CompanyName { get; set; }
+        public string CompanyRegistrationNumber { get; set; }
+        public string OverseasTerritory { get; set; }
+        public string OverseasNumberInTheUnitedKingdom { get; set; }
+    }
+
+    public class PostalAddress
+    {
+
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string AddressLine4 { get; set; }
+        public string City { get; set; }
+        public string County { get; set; }
+        public string Country { get; set; }
+        public string Postcode { get; set; }
+
+        public CareOfAddressType CareOfAddressType { get; set; }
+
+    }
+
+    public class CareOfAddressType
+    {
+        public string CareOfName { get; set; }
+        public string CareOfReference { get; set; }
+    }
+
+    public class AdditionalAddresses
+    {
+
+        public PostalAddress PostalAddress { get; set; }
+        public EmailAddress EmailAddress { get; set; }
+        public DXAddress DXAddress { get; set; }
+    }
+
+    public class EmailAddress
+    {
+        public string Email { get; set; }
+    }
+
+    public class DXAddress
+    {
+
+        public CareOfAddressType CareOfAddressType { get; set; }
+        public string DXNumber { get; set; }
+        public string DXExchange { get; set; }
+    }
+
+}
---
+++ Backend/eDRSUnitTest/LeaseExtensionTest.cs
@@ -0,0 +1,188 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LrApiManager.SOAPManager;
+using LrApiManager.SOAPManager.Restriction;
+using LrApiManager.SOAPManager.LeaseExtension;
+using LrApiManager.XMLClases;
+using LrApiManager.XMLClases.LeaseExtension;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace eDRSUnitTest
+{
+    [TestClass]
+    public class LeaseExtensionTest
+    {
+
+
+        [TestMethod]
+        public void ApplicationRequest()
+        {
+            LeaseExtensionRequestManager LeaseExtensionRequestManager = new LeaseExtensionRequestManager();
+
+
+            Lessortitles lessortitles = new Lessortitles();
+
+            lessortitles.TitleNumber = "GR518197";
+
+            Leaseextension LeaseExtension = new Leaseextension
+            {
+                LessorTitles = lessortitles,
+                LesseeTitle = "LesseeTitle"
+
+            };
+
+            Titles titles1 = new Titles
+            {
+                LeaseExtension = LeaseExtension
+            };
+
+
+            //APPLICATIONS
+            Otherapplication otherapplication = new Otherapplication
+            {
+                Priority = 1,
+                Value = 0,
+                FeeInPence = 500,
+                Document = new Document
+                {
+
+                    CertifiedCopy = "Original"
+                },
+                Type = "RX1"
+
+            };
+
+            List<Otherapplication> otherapplications = new List<Otherapplication>();
+            otherapplications.Add(otherapplication);
+
+            //SUPPORTING DOCUMENTS
+            Supportingdocument Supportingdocument = new Supportingdocument
+            {
+
+
+                CertifiedCopy = "Original",
+                DocumentId = 2,
+                DocumentName = "Evidence"
+            };
+
+            List<Supportingdocument> supportingdocuments = new List<Supportingdocument>();
+            supportingdocuments.Add(Supportingdocument);
+
+
+            //LODGINGCONVENYANCER
+            Lodgingconveyancer lodgingconveyancer = new Lodgingconveyancer
+            {
+
+                RepresentativeId = 1
+            };
+
+            Representations representations = new Representations
+            {
+
+                LodgingConveyancer = lodgingconveyancer,
+                Certified = new Certified
+                {
+                    RepresentativeId = 2
+                }
+            };
+
+
+
+            // PARTY
+            Role role = new Role
+            {
+                RoleType = "ThirdParty",
+                Priority = 1
+            };
+
+            List<Role> roles1 = new List<Role>();
+            roles1.Add(role);
+
+            //Parties
+            Party party = new Party
+            {
+
+                IsApplicant = true,
+                Company = new Company
+                {
+                    CompanyName = "Abbey National PLC"
+                },
+                Roles = roles1
+            };
+
+
+            List<Party> parties = new List<Party>();
+            parties.Add(party);
+
+
+
+
+
+            LeaseExtensionApplicationRequest restrictionApplicationRequest = new LeaseExtensionApplicationRequest
+            {
+
+                AdditionalProviderFilter = "Solsdotcom",
+                MessageId = "scenario4",
+                ExternalReference = "CP/Barclaycard/Murphy",
+
+                Product = new Product
+                {
+                    Reference = "CP/Barclaycard/Murphy",
+                    TotalFeeInPence = 5000,
+                    Email = "carolparker@cozyconveynacers.com",
+                    TelephoneNumber = 1780299299,
+                    AP1WarningUnderstood = true,
+                    ApplicationDate = "2012-02-08",
+                    DisclosableOveridingInterests = false,
+                    Titles = titles1,
+                    Applications = otherapplications,
+                    SupportingDocuments = supportingdocuments,
+                    Representations = representations,
+                    Parties = parties,
+                    ApplicationAffects = "WHOLE"
+                }
+
+            };
+
+            ApplicationResponse applicationResponse = LeaseExtensionRequestManager.RequestLeaseExtensionApplication(restrictionApplicationRequest);
+        }
+
+        [TestMethod]
+        public void AttachmentRequest()
+        {
+
+            AttachmentRequestManager restrictionServiceManager = new AttachmentRequestManager();
+
+            var filearray = new byte[] { 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20 };
+
+            AttachmentRequest AttachmentRequest = new AttachmentRequest
+            {
+                AdditionalProviderFilter = "Solsdotcom",
+                MessageId = "MessageId",
+                ExternalReference = "CP/Parrett/Jenkins",
+                ApplicationMessageId = "ApplicationMessageId",
+                ApplicationService = "104",
+                AttachmentId = 0,
+                CertifiedCopy = "Original",
+                Attachment = filearray
+
+            };
+
+
+            AttachmentResponse attachmentResponse = restrictionServiceManager.RequestAttachment(AttachmentRequest);
+
+        }
+
+        [TestMethod]
+        public void PoolRequest()
+        {
+            LeaseExtensionPollRequest LeaseExtensionPollRequest = new LeaseExtensionPollRequest();
+
+            LeaseExtensionPollResponse restrictionPoolResponse = LeaseExtensionPollRequest.PoolRequest("test msg id");
+        }
+
+    }
+}
