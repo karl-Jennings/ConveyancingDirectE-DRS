1c737d73675daacfe60531866301327fcb50a30d
Sachith <sachith.walpita90@gmail.com>
2021-03-15 09:25:31 +0530

    new class

--- Backend/eDrsManagers/ViewModels/ApplicationPollViewModel.cs
+++ Backend/eDrsManagers/ViewModels/ApplicationPollViewModel.cs
@@ -1,11 +1,11 @@
-﻿using System;
+﻿using BusinessGatewayRepositories.EDRSApplication;
+using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace eDrsManagers.ViewModels
 {
-    public class ApplicationPollRequest
-    {
+    
         public class ApplicationPollRequest
         {
             public string MessageId { get; set; }
@@ -14,5 +14,20 @@ namespace eDrsManagers.ViewModels
 
 
         }
-    }
+
+        public class ResponsePollRequest
+        {
+            public ResponsePollRequest() { }
+            public string Error { get; set; }
+            public string UniqueReference { get; set; }
+            public decimal ActualPrice { get; set; }
+            public bool Successful { get; set; }
+            public string MessageDetails { get; set; }
+            public DateTime ExpiryDate { get; set; }
+            public string UserName { get; set; }
+            public bool Attachment { get; set; }
+            public ResponseApplicationToChangeRegisterV1_0Type GatewayResponse { get; set; }
+
+        }
+    
 }
