64c13a6deb1489a5123e71593e383b9d79246421
Dushyanth <dushyanth@accura-tech.com>
2021-04-28 12:04:24 +0530

    create user

--- Backend/eDrsManagers/ViewModels/UserViewModel.cs
+++ Backend/eDrsManagers/ViewModels/UserViewModel.cs
@@ -10,13 +10,14 @@ namespace eDrsManagers.ViewModels
         public long UserId { get; set; }
         public string Firstname { get; set; }
         public string Lastname { get; set; }
+        public string Username { get; set; }
         public string Email { get; set; }
         public string Designation { get; set; }
         public string Password { get; set; }
         public DateTime CreatedDate { get; set; }
         public DateTime UpdatedDate { get; set; }
         public bool Status { get; set; }
-        public ICollection<DocumentReferenceViewModel> DocumentReferences { get; set; }
+        public virtual ICollection<DocumentReferenceViewModel> DocumentReferences { get; set; }
         public string Fullname { get; set; }
         public bool IsUserValid { get; set; }
         public string Token { get; set; }
