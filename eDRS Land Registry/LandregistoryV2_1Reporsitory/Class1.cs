fe4f1719a9986861a0485c6dca7d8d8161189c0f
Sachith <sachith.walpita90@gmail.com>
2021-09-25 22:55:22 +0530

    -

---
+++ eDRS Land Registry/LandregistoryV2_1Reporsitory/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LandregistoryV2_1Reporsitory
+{
+    public class Class1
+    {
+    }
+}
